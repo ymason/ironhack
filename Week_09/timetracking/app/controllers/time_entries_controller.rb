2bca037af5a4adda2a90b24f9558254e1593f568
Owner <owner@MacBook-Pro-3.local>
2016-07-02 15:06:03 -0400

    Added functionality for time entries and added error messages.

--- Week_09/timetracking/app/controllers/time_entries_controller.rb
+++ Week_09/timetracking/app/controllers/time_entries_controller.rb
@@ -1,5 +1,22 @@
 class TimeEntriesController < ApplicationController
 
+	def create
+		#input: params[:project_id] and params[:time_entry]
+		@project = Project.find params[:project_id]
+
+		#permit is whitelisting only those attributes to be assigned
+		time_entry_params = params.require(:time_entry).permit(:hous, :minutes, :date)
+
+		# without permit: @project.time_entries.new params[:time_entry]
+
+		@time_entry = @project.time_entries.new(time_entry_params)
+		if @time_entry.save
+			redirect_to project_time_entries_path(@project)
+		else
+			render 'new'
+		end
+	end
+
 	def index
 	@project = Project.find(params[:project_id])
 	@time_entries = @project.time_entries
